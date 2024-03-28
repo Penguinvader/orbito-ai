798611fed1a163dc716e6684713ac2b0da868df3
Balazs Bartha <balifej@gmail.com>
2024-03-28 17:31:00 +0100

    minimax changes

--- minimax.py
+++ minimax.py
@@ -3,14 +3,14 @@ from state import State
 
 def minimax(state: State, depth: int, jt: int):
     if depth == 0 or state.h0 == 3:
-        return state.evaluate(jt), 'end'
+        return state.evaluate(jt), state.last_move
 
-    available_states = [s[1] for s in state.state_tree_depth_1()]
+    available_states = [(s[1], minimax(s[1], depth-1, jt)) for s in state.state_tree_depth_1()]
 
     if state.jt == jt:
-        best_state = max(available_states, key=lambda s: minimax(s, depth-1, jt)[0])
-        return best_state.evaluate(jt), best_state.last_move
+        best = max(available_states, key=lambda s: s[1][0])
+        return best[1][0], best[0].last_move
     elif state.jt == 3-jt:
-        worst_state = min(available_states, key=lambda s: minimax(s, depth - 1, jt)[0])
-        return worst_state.evaluate(jt), worst_state.last_move
+        worst = min(available_states, key=lambda s: s[1][0])
+        return worst[1][0], worst[0].last_move
 
