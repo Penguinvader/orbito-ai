f655b9eafa2ac6f4a8a54c57aba6cb714a20ce43
Balazs Bartha <balifej@gmail.com>
2024-03-29 18:41:57 +0100

    alpha beta pruning fixed maybe

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ import minimax
 if __name__ == '__main__':
     a = State()
     # a.h0 = 1
-    # a.h = [[0, 2, 0, 0],
-    #        [2, 0, 0, 1],
-    #        [1, 0, 0, 2],
-    #        [0, 0, 1, 0]]
+    # a.h = [[0, 2, 2, 0],
+    #        [2, 2, 0, 2],
+    #        [2, 2, 0, 2],
+    #        [0, 2, 2, 0]]
+    # print(evaluators.defensive_three(a, 1))
 
     ai_mode = 1
     while a.h0 != 3:
@@ -23,7 +24,7 @@ if __name__ == '__main__':
             player = 'white' if a.jt == 1 else 'black'
             print(f'Player {player} {'move' if a.h0 == 1 else 'placement'} coordinates (row column):')
             if player == 'white' and ai_mode == 1:
-                mm = minimax.minimax_ab(a, 2, 1, evaluator=evaluators.three_in_a_row)
+                mm = minimax.minimax(a, 2, 1, evaluator=evaluators.defensive_three)
                 move = mm[1]
                 print(mm)
                 if move == 'skip':
--- minimax.py
+++ minimax.py
@@ -13,12 +13,12 @@ def minimax(state: State, depth: int, jt: int, evaluator=evaluators.win):
     if state.jt == jt:
         best = max(child_nodes, key=lambda s: s[1][0])
         return best[1][0], best[0].last_move
-    elif state.jt == 3 - jt:
+    else:
         worst = min(child_nodes, key=lambda s: s[1][0])
         return worst[1][0], worst[0].last_move
 
 
-def minimax_ab(state: State, depth: int, jt: int, alpha=(-1000.0, 'alpha'), beta=(1000, 'beta'),
+def minimax_ab(state: State, depth: int, jt: int, alpha=-1000.0, beta=1000.0,
                evaluator=evaluators.win):
     return alpha_beta(state, depth, jt, alpha, beta, evaluator=evaluator)
 
@@ -33,17 +33,17 @@ def alpha_beta(state: State, depth: int, jt: int, alpha, beta, evaluator=evaluat
         best_val = -1000.0, 'default max'
         for node in child_nodes:
             value = alpha_beta(node[1], depth - 1, jt, alpha, beta, evaluator)
-            best_val = max(value, best_val, key=lambda n: n[0])
-            alpha = max(alpha, best_val, key=lambda n: n[0])
-            if beta[0] <= alpha[0]:
+            best_val = max(value[0], best_val[0]), node[0]
+            alpha = max(alpha, best_val[0])
+            if beta <= alpha:
                 break
         return best_val
-    elif state.jt == 3 - jt:
+    else:
         worst_val = 1000.0, 'default min'
         for node in child_nodes:
             value = alpha_beta(node[1], depth - 1, jt, alpha, beta, evaluator)
-            worst_val = min(value, worst_val, key=lambda n: n[0])
-            beta = min(alpha, worst_val, key=lambda n: n[0])
-            if beta[0] <= alpha[0]:
+            worst_val = min(value[0], worst_val[0]), node[0]
+            beta = min(alpha, worst_val[0])
+            if beta <= alpha:
                 break
         return worst_val
